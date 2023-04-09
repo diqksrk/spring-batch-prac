1f74199cf10b111eff510c0186c7d41e5729706c
minjun.kang <minjun.kang@navercorp.com>
2023-04-10 00:30:35 +0900

    init

---
+++ src/test/java/com/example/springbatchprac/SpringBatchPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
